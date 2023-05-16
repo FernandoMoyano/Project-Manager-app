19181742046e42db44db5d5b1c37ddb7142ef4bd
FernandoMoyano <fernandomoyano21@gmail.com>
2023-05-16 19:09:45 -0300

    PriorityDisplay

--- src/components/PriorityDisplay/PriorityDisplay.jsx
+++ src/components/PriorityDisplay/PriorityDisplay.jsx
@@ -1,7 +1,22 @@
+import PropTypes from "prop-types";
 import "./PriorityDisplay.css";
 
 const PriorityDisplay = ({ priority }) => {
-	return <div>PriorityDisplay</div>;
+	return (
+		<div className="priority-display">
+			<div className="star-container">
+				<h3 style={{ color: priority >= 1 ? "#FFC300" : "" }}>★</h3>
+				<h3 style={{ color: priority >= 2 ? "#FFC300" : "" }}>★</h3>
+				<h3 style={{ color: priority >= 3 ? "#FFC300" : "" }}>★</h3>
+				<h3 style={{ color: priority >= 4 ? "#FFC300" : "" }}>★</h3>
+				<h3 style={{ color: priority >= 5 ? "#FFC300" : "" }}>★</h3>
+			</div>
+		</div>
+	);
+};
+
+PriorityDisplay.propTypes = {
+	priority: PropTypes.number.isRequired,
 };
 
 export default PriorityDisplay;
