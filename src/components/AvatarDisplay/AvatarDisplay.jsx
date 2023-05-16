e8bc6504e2d8401ec7d5219ccdbcd9ff3849e1e5
FernandoMoyano <fernandomoyano21@gmail.com>
2023-05-16 17:44:46 -0300

    AvatarDisplay

--- src/components/AvatarDisplay/AvatarDisplay.jsx
+++ src/components/AvatarDisplay/AvatarDisplay.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import "./AvatarDisplay.css";
+import avatarDefault from "../../assets/images/avatarDefault.svg"
+
 
 
 
@@ -7,7 +9,7 @@ const AvatarDisplay = ({ticket}) => {
   return (
     <div className="avatar-container">
       <div className="image-container">
-      <img src={ticket.avatar ? ticket.avatar : null} alt={'photo of ' + ticket.owner} />
+      <img src={ticket.avatar ? ticket.avatar :avatarDefault } alt={'photo of ' + ticket.owner} />
       </div>
     </div>
   )
--- src/components/StatusDisplay/StatusDisplay.jsx
+++ src/components/StatusDisplay/StatusDisplay.jsx
@@ -1,8 +1,13 @@
-
-const StatusDisplay = () => {
+import PropTypes from "prop-types";
+const StatusDisplay = ({status}) => {
   return (
     <div>StatusDisplay</div>
   )
 }
+
+StatusDisplay.propTypes = {
+	status: PropTypes.object.isRequired,
+	
+};
 
 export default StatusDisplay
